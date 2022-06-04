c2ed4f68937221194d62d2a6f969dc8bcf8156d6
jiahhou <jhhou@buffalo.edu>
2022-06-04 03:37:29 -0400

    added profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image } from '@chakra-ui/react'
 
 const Page = () => {
   return (
@@ -13,6 +13,23 @@ const Page = () => {
       	    Jia Hou
           </Heading>
 	        <p>Developer ( Front-end / Full-stack / Software )</p>
+        </Box>
+        <Box 
+          flexShrink={0} 
+          mt={{base: 4, md: 0}} 
+          ml={{md: 6}} 
+          align="center"
+        >
+          <Image 
+            borderColor="whiteAlpha.800" 
+            borderWidth={2} 
+            borderStyle="solid" 
+            maxWidth="100px" 
+            display="inline-block" 
+            borderRadius="full" 
+            src="/images/jia.jpg" 
+            alt="Image of Jia"
+          />
         </Box>
       </Box>
     </Container>
