c40d4a98fc484997754885f732c2ecaaa1c2e47e
jiahhou <jhhou@buffalo.edu>
2022-06-07 00:23:30 -0400

    added more description for my work section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
+import NextLink from 'next/link'
 import { 
   Container, 
   Box, 
   Heading, 
   Image, 
-  useColorModeValue 
+  useColorModeValue, 
+  Button,
+  Link
 } from '@chakra-ui/react'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const Page = () => {
   return (
@@ -51,7 +55,28 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <Paragraph>lorem ipsum </Paragraph>
+        <Paragraph>Jia is a recent graduate at the University at Buffalo and aspires to 
+        become a web/software developer. He has a passion for building software, 
+        websites and solving problems. Offline, he loves playing volleyball, weightlifting, 
+        rock climbing and recently got into leetcoding. In his senior year he took many web 
+        development courses and created 
+          <NextLink href="/works/studyseeker">
+            <Link> Study Seeker </Link>
+          </NextLink>
+        in his Applied Human Computer Interaction and Interface Design class (CSE370) and 
+          <NextLink href="/works/mosaicmaker">
+            <Link> Mosaic Maker </Link>
+          </NextLink>
+        in his Software Engineering class (CSE442). It was these projects that really inspired 
+        him to pursue a path in web/software development.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   )
