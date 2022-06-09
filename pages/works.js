b58021e2db45a1d90bc10edfc5b34de58a9e3a4f
jiahhou <jhhou@buffalo.edu>
2022-06-09 00:57:14 -0400

    fix formatting for projects in works.js

--- pages/works.js
+++ pages/works.js
@@ -20,9 +20,13 @@ const Works = () => {
         Works
       </Heading>
 
-      <SimpleGrid columns={[1,1,2]} gap={6}>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="mosaic-maker" title="Mosaic Maker" thumbnail={thumbMosaic}>
+          <WorkGridItem 
+            id="mosaic-maker" 
+            title="Mosaic Maker" 
+            thumbnail={thumbMosaic}
+          >
             <strong>CSE 442 (Software Engineering)</strong>
             <br/>
             <i>2nd Place in CSE Demo Day</i>
@@ -31,7 +35,11 @@ const Works = () => {
           </WorkGridItem>
         </Section>
         <Section>
-          <WorkGridItem id="study-seeker" title="Study Seeker" thumbnail={thumbStudy}>
+          <WorkGridItem 
+            id="study-seeker" 
+            title="Study Seeker" 
+            thumbnail={thumbStudy}
+          >
             <strong>CSE 370 (HCI and Interface Design)</strong>
             <br/>
             Social media platform to build a student community, nurturing growth and helping students succeed by aligning academic interests and goals
