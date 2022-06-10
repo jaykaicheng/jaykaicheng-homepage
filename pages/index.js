9fb308e2b4232cc2f66de42cc8eb92e5e5a4bc13
jiahhou <jhhou@buffalo.edu>
2022-06-10 00:13:47 -0400

    add animations for changing pages

--- pages/index.js
+++ pages/index.js
@@ -12,112 +12,115 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container>
-      <Box 
-        borderRadius="lg" 
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
-        p={3} 
-        mb={6} 
-        align="center"
-      >
-	  Hello, I&apos;m a front-end developer based in Brooklyn, New York!
-      </Box>
-
-      <Box display={{md:'flex'}}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-      	    Jia Hou
-          </Heading>
-	        <p>Developer ( Front-end / Full-stack / Software )</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box 
-          flexShrink={0} 
-          mt={{base: 4, md: 0}} 
-          ml={{md: 6}} 
+          borderRadius="lg" 
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
+          p={3} 
+          mb={6} 
           align="center"
         >
-          <Image 
-            borderColor="whiteAlpha.800" 
-            borderWidth={2} 
-            borderStyle="solid" 
-            maxWidth="100px" 
-            display="inline-block" 
-            borderRadius="full" 
-            src="/images/jia.jpg" 
-            alt="Image of Jia"
-          />
+      Hello, I&apos;m a front-end developer based in Brooklyn, New York!
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>Jia is a recent graduate at the University at Buffalo and aspires to 
-        become a web/software developer. He has a passion for building software, 
-        websites and solving problems. Offline, he spends his time playing volleyball, weightlifting
-        and recently got into leetcoding. In his senior year he took many web 
-        development courses and created{' '} 
-          <NextLink href="/works/studyseeker">
-            <Link>Study Seeker</Link>
-          </NextLink> 
-        {' '}in his Applied Human Computer Interaction and Interface Design class (CSE370) and 
-        {' '}
-          <NextLink href="/works/mosaicmaker">
-            <Link>Mosaic Maker</Link>
-          </NextLink>
-        {' '} in his Software Engineering class (CSE442). These projects inspired 
-        him to pursue a career in web/software development.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{md:'flex'}}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Jia Hou
+            </Heading>
+            <p>Developer ( Front-end / Full-stack / Software )</p>
+          </Box>
+          <Box 
+            flexShrink={0} 
+            mt={{base: 4, md: 0}} 
+            ml={{md: 6}} 
+            align="center"
+          >
+            <Image 
+              borderColor="whiteAlpha.800" 
+              borderWidth={2} 
+              borderStyle="solid" 
+              maxWidth="100px" 
+              display="inline-block" 
+              borderRadius="full" 
+              src="/images/jia.jpg" 
+              alt="Image of Jia"
+            />
+          </Box>
         </Box>
-      </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1999</BioYear>
-          Born in Guangdong (广东), China.
-        </BioSection>
-        <BioSection>
-          <BioYear>2003</BioYear>
-          Immigrated to America.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>
-          Graduated from Brooklyn Technical High School after majoring in Architecture.
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Completed Bachelors program in the Undergraduate School of Engineering and Applied Sciences 
-          from the University at Buffalo after majoring in Computer Science and minoring in Psychology.
-        </BioSection>
-        <BioSection>
-          <BioYear>Present</BioYear>
-          Searching for an opportunity to learn in a fast-paced and collaborative setting.
-        </BioSection>
-      </Section>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>Jia is a recent graduate at the University at Buffalo and aspires to 
+          become a web/software developer. He has a passion for building software, 
+          websites and solving problems. Offline, he spends his time playing volleyball, weightlifting
+          and recently got into leetcoding. In his senior year he took many web 
+          development courses and created{' '} 
+            <NextLink href="/works/studyseeker">
+              <Link>Study Seeker</Link>
+            </NextLink> 
+          {' '}in his Applied Human Computer Interaction and Interface Design class (CSE370) and 
+          {' '}
+            <NextLink href="/works/mosaicmaker">
+              <Link>Mosaic Maker</Link>
+            </NextLink>
+          {' '} in his Software Engineering class (CSE442). These projects inspired 
+          him to pursue a career in web/software development.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Volleyball, Bouldering, Weightlifting, Walks, Spending time with friends and family, Driving
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1999</BioYear>
+            Born in Guangdong (广东), China.
+          </BioSection>
+          <BioSection>
+            <BioYear>2003</BioYear>
+            Immigrated to America.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>
+            Graduated from Brooklyn Technical High School after majoring in Architecture.
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Completed Bachelors program in the Undergraduate School of Engineering and Applied Sciences 
+            from the University at Buffalo after majoring in Computer Science and minoring in Psychology.
+          </BioSection>
+          <BioSection>
+            <BioYear>Present</BioYear>
+            Searching for an opportunity to learn in a fast-paced and collaborative setting.
+          </BioSection>
+        </Section>
 
-        </Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Volleyball, Bouldering, Weightlifting, Walks, Spending time with friends and family, Driving
+
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -9,44 +9,47 @@ import { WorkGridItem } from '../components/grid-item'
 
 import thumbMosaic from '../public/images/mosaic-maker.png'
 import thumbStudy from '../public/images/study-seeker.png'
+import Layout from '../components/layouts/article'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" 
-        fontSize={20} 
-        mb={4}
-      >
-        Works
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" 
+          fontSize={20} 
+          mb={4}
+        >
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem 
-            id="mosaic-maker" 
-            title="Mosaic Maker" 
-            thumbnail={thumbMosaic}
-          >
-            <strong>CSE 442 (Software Engineering)</strong>
-            <br/>
-            <i>2nd Place in CSE Demo Day</i>
-            <br/>
-            Generate mosaic images without knowledge of photo-editing skills for free and without watermarks
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem 
-            id="study-seeker" 
-            title="Study Seeker" 
-            thumbnail={thumbStudy}
-          >
-            <strong>CSE 370 (HCI and Interface Design)</strong>
-            <br/>
-            Social media platform to build a student community, nurturing growth and helping students succeed by aligning academic interests and goals
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem 
+              id="mosaic-maker" 
+              title="Mosaic Maker" 
+              thumbnail={thumbMosaic}
+            >
+              <strong>CSE 442 (Software Engineering)</strong>
+              <br/>
+              <i>2nd Place in CSE Demo Day</i>
+              <br/>
+              Generate mosaic images without knowledge of photo-editing skills for free and without watermarks
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem 
+              id="study-seeker" 
+              title="Study Seeker" 
+              thumbnail={thumbStudy}
+            >
+              <strong>CSE 370 (HCI and Interface Design)</strong>
+              <br/>
+              Social media platform to build a student community, nurturing growth and helping students succeed by aligning academic interests and goals
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
