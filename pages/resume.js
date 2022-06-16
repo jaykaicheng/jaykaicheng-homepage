8f2e691252d68e91a89781d05e0799c18722c7e7
jiahhou <jhhou@buffalo.edu>
2022-06-16 01:56:57 -0400

    add pdf of resume

--- pages/resume.js
+++ pages/resume.js
@@ -7,8 +7,17 @@ import Section from '../components/section'
 
 import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 
 const Resume = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
     <Layout>
       <Container>
@@ -20,9 +29,16 @@ const Resume = () => {
         >
           Resume
         </Heading>
-        <Paragraph>
-
-        </Paragraph>
+      
+        <div>
+          <Document file="../public/Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+        </div>
+        
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
           </Section>
